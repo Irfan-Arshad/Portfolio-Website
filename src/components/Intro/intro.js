cf77fde471afc77f58008bc6d02d54f7b9455682
Irfan-Arshad <ify10_arshad@hotmail.co.uk>
2024-02-01 18:42:44 +0000

    added cv download button

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,21 +2,27 @@ import React from 'react';
 import './intro.css';
 import bg from '../../assets/Me.png';
 import btnImg from '../../assets/hireme.png';
+import cv from '../../assets/Irfan Arshad - Developer - CV - 2024.pdf'
 // import { Link } from 'react-scroll';
 
 const Intro = () => {
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello World, </span>
-            <span className="introText">I'm <span className="introName">Irfan</span> <br />Graduate Developer</span>
-            <p className="introPara"> I'm an enthusiastic recent graduate with a strong educational foundation <br/> and a passion for launching my career in the field of software / web development.</p>
-            <button className='btn' onClick={() => {
-              document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
-              }}>
+      <div className="introContent">
+        <span className="hello">Hello World, </span>
+        <span className="introText">I'm <span className="introName">Irfan</span> <br />Graduate Developer</span>
+        <p className="introPara"> I'm an enthusiastic recent graduate with a strong educational foundation <br /> and a passion for launching my career in the field of software / web development.</p>
+        <div>
+          <button className='btn' onClick={() => {
+            document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+          }}>
             <img src={btnImg} alt="Hire Me" className='btnImg' />Hire Me</button>
+          <a href={cv} download="Irfan Arshad - Developer - CV - 2024.pdf">
+            <button className='cvBtn' >Download CV</button>
+          </a>
         </div>
-        <img src={bg} alt="Profile" className="bg" />
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
     </section>
   );
 }
